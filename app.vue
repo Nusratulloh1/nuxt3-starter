a3ae7f1d5fad93c1d430462f6ae28480bb25e696
Nusratulloh1 <axunovnusrat@gmail.com>
2023-06-03 12:29:49 +0500

    app.vue update

---
+++ app.vue
@@ -0,0 +1,24 @@
+<template>
+    <NuxtLayout>
+      <div>
+        <NuxtPage />
+      </div>
+    </NuxtLayout>
+  </template>
+  
+  <script lang="ts" setup>
+  import { onMounted } from 'vue'
+  import { useRoute } from 'vue-router'
+  
+  const route = useRoute()
+  
+  if ('setup' in route.query) {
+    throw new Error('error in setup')
+  }
+  if ('mounted' in route.query) {
+    onMounted(() => {
+      throw new Error('error in mounted')
+    })
+  }
+  
+  </script>
